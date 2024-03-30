1200d54cb9727d411f39faf95ad667dfb4e77718
dharmanshu <dharmanshu4@gmail.com>
2024-03-30 11:06:54 +0530

    Updation

--- farm-api/Repository/Connections.cs
+++ farm-api/Repository/Connections.cs
@@ -11,7 +11,7 @@ namespace Repository
         public static string Connect()
         {
             return "Data Source=LAPTOP-7Q3NO3O1\\SQLEXPRESS;Initial Catalog=Farm;Trusted_Connection=True;";
-            //return "Data Source=SQL8001.site4now.net;Initial Catalog=db_a85a28_scouts;User Id=db_a85a28_scouts_admin;Password=scouts@123;";
+            //return "Data Source=SQL8001.site4now.net;Initial Catalog=db_a85a28_scouts;User Id=db_a85a28_scouts_admin;Password=f***@**3;";
         }
     }
 }
